415c2614c02186393809d500dc0fa5916c87946f
sa1123 <sahil2398@yahoo.com>
2022-03-06 19:14:12 +0530

    index css

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,17 @@
-<h1>Blog</h1>
+<script>
+    let title = 'Blog';
+</script>
+
+<div class="index">
+    <h1>{title}</h1>
+</div>
+
+<style>
+    .index{
+        text-align: center;
+        display: block;
+        margin: 20px auto;
+        color: salmon;
+        background-color: darkslateblue;
+    }
+</style>
