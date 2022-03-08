2f64f8e6face96da6837caf042133491dc97ce0b
sa1123 <sahil2398@yahoo.com>
2022-03-08 22:09:24 +0530

    prefetch

--- src/routes/guides/[id].svelte
+++ src/routes/guides/[id].svelte
@@ -1,5 +1,6 @@
 <script context='module'>
     export async function load({ fetch, params }) {
+        await new Promise(resolve => setTimeout(resolve, 1000))
         const id = params.id
         const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
         const guide = await res.json()
