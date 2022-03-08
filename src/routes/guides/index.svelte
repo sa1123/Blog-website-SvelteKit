4305e25f0eff68075f583f5d69f8cfdedaf76f53
sa1123 <sahil2398@yahoo.com>
2022-03-08 22:02:52 +0530

    dynamic routing

--- src/routes/guides/index.svelte
+++ src/routes/guides/index.svelte
@@ -2,6 +2,7 @@
     export async function load({fetch}) {
         const res = await fetch('https://jsonplaceholder.typicode.com/posts')
         const guides = await res.json()
+        console.log(guides)
 
         if(res.ok){
             return {
@@ -19,9 +20,6 @@
 </script>
 
 <script>
-import { each } from "svelte/internal";
-
-
     export let guides
 </script>
 
@@ -29,7 +27,7 @@ import { each } from "svelte/internal";
     <ul>
         {#each guides as guide}
             <li>
-                <a href="/">{guide.title}</a>
+                <a href={`/guides/${guide.id}`}>{guide.title}</a>
             </li>
         {/each}
     </ul>
